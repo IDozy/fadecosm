282b3e6017fe31391ee806a20975ff9d95342c61
gsoonc <109822183+gsoonc@users.noreply.github.com>
2023-09-05 19:09:57 -0500

    Agregación del componente a  App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
+import Contact from './components/Contact';
 
 const App = () =>{
   return(
