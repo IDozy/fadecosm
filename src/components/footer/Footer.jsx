8a404945d23e0cbed1e48df25b9206edfafa38a6
Dozy <ahuatayc18_2@unc.edu.pe>
2023-09-10 21:44:02 -0500

    Links footer e info

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import "./footer.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -71,29 +71,96 @@ const Footer = () => {
                         </p>
                     </div>
                 </div>
-                {/*<div className="sb__footer-below-links">
-                    <a href="">
-                        <div>
-                            <p>termios y condiciones</p>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div>
-                            <p>privacy</p>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div>
-                            <p>security</p>
-                        </div>
-                    </a>
-
-    </div>*/}
+               
 
             </div>
 
         </div>
     )
+}
+
+export default Footer;
+*/import React from 'react';
+import "./footer.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+    const year = new Date().getFullYear();
+
+    const links = [
+        { title: "Fadeco San Martin", items: ["Nos dedicamos a la fabricación de productos de concreto prefabricado para el sector industrial y saneamiento.", "Contamos con más de 15 años de experiencia.", "Lunes a viernes:  8.00 am a 5.00 pm. Sábados: 8.00 am a 12.00 pm"] },
+        { title: "Nuestros Productos", items: ["– Buzones", "– Cajas de registro para desagüe", "– Caja porta medidor de agua", "– Caja condominial" ,"– Marco de fierro fundido"] },
+        { title: "Contactanos", items: ["link", "link", "link"] },
+    ];
+
+    const socialMediaIcons = [
+        { icon: faFacebook },
+        { icon: faTwitter },
+        { icon: faInstagram },
+        { icon: faLinkedin },
+    ];
+
+    const renderLinks = (title, items) => (
+        <div className="sb__footer-links-div">
+            <h4>{title}</h4>
+            {items.map((item, index) => (
+                <a href={`/${item}`} key={index}>
+                    <p>{item}</p>
+                </a>
+            ))}
+        </div>
+    );
+
+    const renderSocialMediaIcons = () => (
+        <div className="sb__footer-links-div">
+            <h4>Síguenos</h4>
+            <div className="socialmedia">
+                {socialMediaIcons.map((item, index) => (
+                    <p>
+                    <a href={getSocialMediaLink(item.icon)} key={index} target="_blank" rel="noopener noreferrer">
+                        <FontAwesomeIcon icon={item.icon} />
+                    </a>
+                    </p>
+                ))}
+            </div>
+        </div>
+    );
+
+   
+    const getSocialMediaLink = (icon) => {
+        switch (icon) {
+            case faFacebook:
+                return "https://www.facebook.com/TuPaginaDeFacebook";
+            case faTwitter:
+                return "https://twitter.com/TuCuentaDeTwitter";
+            case faInstagram:
+                return "https://www.instagram.com/TuCuentaDeInstagram";
+            case faLinkedin:
+                return "https://www.linkedin.com/in/TuPerfilDeLinkedIn";
+            default:
+                return "#";
+        }
+    };
+
+    return (
+        <div className='footer'>
+            <div className="sb__footer section__padding">
+                <div className="sb__footer-links">
+                    {links.map((link, index) => (
+                        renderLinks(link.title, link.items)
+                    ))}
+                    {renderSocialMediaIcons()}
+                </div>
+                <hr />
+                <div className="sb__footer-below">
+                    <div className="sb__footer-copyright">
+                        <p>@{year} Dozgson. All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
