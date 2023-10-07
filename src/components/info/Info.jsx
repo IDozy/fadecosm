f8f6a64f7907ec0830e6ca661b9980d78e3d205b
gsoonc <109822183+gsoonc@users.noreply.github.com>
2023-10-07 14:23:04 -0500

    texto a las cards

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -8,12 +8,12 @@ const Info = () => {
     {
       icon: faRankingStar,
       title:"CÁLIDAD",
-      text:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quas minima fugit accusamus asperiores repellendus cum recusandae hic corporis mollitia molestiae, in deleniti saepe ab vel, neque delectus assumenda distinctio.`
+      text:`En nuestra fábrica, nos destacamos por mantener los más rigurosos estándares de calidad, de acuerdo con las normativas establecidas por los organismos de supervisión en la industria de la construcción. Nuestro equipo altamente capacitado realiza una evaluación exhaustiva de nuestros productos en todas las etapas del proceso de fabricación.`
     },
     {
       icon: faPeopleGroup,
       title: "TRABAJO EN EQUIPO",
-      text:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque quas minima fugit accusamus asperiores repellendus cum recusandae hic corporis mollitia molestiae, in deleniti saepe ab vel, neque delectus assumenda distinctio.`
+      text:`Valoramos la colaboración y la sinergia, lo que nos permite lograr resultados excepcionales en prefabricados. Elegirnos significa optar por un equipo unido y comprometido que trabajará incansablemente para cumplir con sus necesidades en cada proyecto.`
     }
   ])
   return (
