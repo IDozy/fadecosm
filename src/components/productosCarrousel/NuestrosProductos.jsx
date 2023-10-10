d306c45fc0bc4633a131984cb6f09cc8d4de2e8c
gsoonc <109822183+gsoonc@users.noreply.github.com>
2023-10-09 20:00:59 -0500

    correcion de nuestros productos

--- src/components/productosCarrousel/NuestrosProductos.jsx
+++ src/components/productosCarrousel/NuestrosProductos.jsx
@@ -1,32 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./NuestrosProductos.css";
-
 import img1 from "../../images/Elaboracion/Elaboracion1.jpg";
 import img2 from "../../images/Elaboracion/Elaboracion2.jpg";
 import img3 from "../../images/Elaboracion/Elaboracion3.jpg";
 import img4 from "../../images/Elaboracion/Elaboracion4.jpg";
 import img5 from "../../images/Elaboracion/Elaboracion5.jpg";
-import img6 from "../../images/Elaboracion/Elaboracion6.jpg";
+import img6 from "../../images/Elaboracion/ELaboracion6.png";
 import img7 from "../../images/Elaboracion/Elaboracion7.jpg";
 import img8 from "../../images/Elaboracion/Elaboracion8.jpg";
+import img9 from '../../images/Elaboracion/Elaboracion9.jpg';
 
 export const NuestrosProductos = () => {
-  const images = [ img1,img2,img3, img4, img5, img6, img7, img8];
-
-  const groupSize = 3;
+  const images = [img1, img2, img3, img4, img5, img6, img7, img8,img9];
+  const groupSize = images.length;
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Avanzar al siguiente índice
       setCurrentIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
     }, 3000); // Cambia de imagen cada 3 segundos (ajusta según tus preferencias)
 
     return () => {
-      clearInterval(interval); // Limpia el intervalo al desmontar el componente
+      clearInterval(interval);
     };
   }, [images.length]);
 
@@ -49,7 +47,7 @@ export const NuestrosProductos = () => {
         <p>
           Son elaborados con un proceso supervisado desde la preparación del
           material, hasta los últimos acabados, contamos con la posibilidad de
-          ajustar tú producto acorde a las medidas solicitadas en tus proyectos.
+          ajustar tu producto acorde a las medidas solicitadas en tus proyectos.
         </p>
       </div>
     </div>
