84acd0b7c2290a8610e3d5c987bd6aadfb921791
gsoonc <109822183+gsoonc@users.noreply.github.com>
2023-10-14 20:17:31 -0500

    Arreglando componente de nuestros productos

--- src/components/productosCarrousel/NuestrosProductos.jsx
+++ src/components/productosCarrousel/NuestrosProductos.jsx
@@ -1,18 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./NuestrosProductos.css";
-import img1 from "../../images/Elaboracion/Elaboracion1.jpg";
-import img2 from "../../images/Elaboracion/Elaboracion2.jpg";
-import img3 from "../../images/Elaboracion/Elaboracion3.jpg";
-import img4 from "../../images/Elaboracion/Elaboracion4.jpg";
-import img5 from "../../images/Elaboracion/Elaboracion5.jpg";
-import img6 from "../../images/Elaboracion/ELaboracion6.png";
-import img7 from "../../images/Elaboracion/Elaboracion7.jpg";
-import img8 from "../../images/Elaboracion/Elaboracion8.jpg";
-import img9 from '../../images/Elaboracion/Elaboracion9.jpg';
+import img from "../../images/Elaboracion/calidad1.jpg";
+import img1 from "../../images/Elaboracion/calidad2.jpg";
+import img2 from "../../images/Elaboracion/Elaboracion1.jpg";
+import img3 from "../../images/Elaboracion/Elaboracion2.jpg";
+
 
 export const NuestrosProductos = () => {
-  const images = [img1, img2, img3, img4, img5, img6, img7, img8,img9];
+  const images = [img,img1,img2,img3 ];
   const groupSize = images.length;
   const [currentIndex, setCurrentIndex] = useState(0);
 
