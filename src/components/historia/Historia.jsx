026e76f90f803ec457cf41ec0df56f67af11febe
Dozy <ahuatayc18_2@unc.edu.pe>
2023-10-16 19:25:54 -0500

    Creacion componente dinamico sectionPh

--- src/components/historia/Historia.jsx
+++ src/components/historia/Historia.jsx
@@ -1,35 +1,24 @@
 import React from "react";
 import "./Historia.css";
 import img1 from "../../images/fotocorp.png";
+import SectionPH from "./SectionPH";
 
 export const Historia = () => {
-  return (
-    <div className="nuestra-historia-card">
-      <div className="nuestra-historia-image">
-        <img src={img1} alt="Imagen de Nuestra Historia" />
-      </div>
-      <div className="nuestra-historia-text">
-        <div className="card-1" style={{margin: "0 5px", paddingRight: "7%", height: "auto" }}>
-          <h2 className="text-title">Nuestra Historia</h2>
-          <h4 className="text-info" style={{marginBottom: ".5rem", paddingLeft: "1rem" }}>Fadedo SM</h4>
-          <p>
-            La fábrica de concreto (FADECO) fue creada en el año 2009 , con el fin
-            de beneficiar a la ciudad de Cajamarca en los servicios de saneamiento
-            y alcantarillado, en sus inicios contaba con dos trabajadores y cuya
-            fábrica de concreto no contaba con máquinas tecnológicas para la
-            elaboración de ladrillos de techo y otros productos de concreto, así
-            que, se elaboraban de una manera precaria, es decir, se realizaban en
-            un tronco de madera por vibración con el fin de elaborar los productos
-            de concreto, de manera paulatina se implementó la fábrica con modernos
-            equipos tecnológicos, ya en el año 2012, el número de trabajadores se
-            incrementó a once , por ende ya no solamente se dedicaron a la
-            elaboración de ladrillo de techo, sino también a otros productos de
-            concreto como son: cajas para desagüe, buzón de desagüe, etc. Al
-            incrementarse el nivel de producción la empresa FADECO, fue siendo
-            reconocida en toda la región de Cajamarca.
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+  const title = "Nuestra Historia";
+  const text = `La fábrica de concreto (FADECO) fue creada en el año 2009 , con el fin
+    de beneficiar a la ciudad de Cajamarca en los servicios de saneamiento
+    y alcantarillado, en sus inicios contaba con dos trabajadores y cuya
+    fábrica de concreto no contaba con máquinas tecnológicas para la
+    elaboración de ladrillos de techo y otros productos de concreto, así
+    que, se elaboraban de una manera precaria, es decir, se realizaban en
+    un tronco de madera por vibración con el fin de elaborar los productos
+    de concreto, de manera paulatina se implementó la fábrica con modernos
+    equipos tecnológicos, ya en el año 2012, el número de trabajadores se
+    incrementó a once , por ende ya no solamente se dedicaron a la
+    elaboración de ladrillo de techo, sino también a otros productos de
+    concreto como son: cajas para desagüe, buzón de desagüe, etc. Al
+    incrementarse el nivel de producción la empresa FADECO, fue siendo
+    reconocida en toda la región de Cajamarca.`;
+
+  return <SectionPH title={title} image={img1} text={text} />;
 };
--- src/components/productosCarrousel/NuestrosProductos.jsx
+++ src/components/productosCarrousel/NuestrosProductos.jsx
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./NuestrosProductos.css";
+
 import img from "../../images/Elaboracion/calidad1.jpg";
 import img1 from "../../images/Elaboracion/calidad2.jpg";
 import img2 from "../../images/Elaboracion/Elaboracion1.jpg";
 import img3 from "../../images/Elaboracion/Elaboracion2.jpg";
 
+import "../../components/historia/Historia.css";
+import SectionPH from "../historia/SectionPH";
+
 
 export const NuestrosProductos = () => {
-  const images = [img,img1,img2,img3 ];
-  const groupSize = images.length;
+  const images = [img, img1, img2, img3];
+  const imageChangeInterval = 3000; 
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -17,35 +19,19 @@ export const NuestrosProductos = () => {
       setCurrentIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
-    }, 3000); // Cambia de imagen cada 3 segundos (ajusta según tus preferencias)
+    }, imageChangeInterval);
 
     return () => {
       clearInterval(interval);
     };
   }, [images.length]);
 
-  const displayImages = images.slice(currentIndex, currentIndex + groupSize);
+  const currentImage = images[currentIndex];
 
-  return (
-    <div className="nuestros-productos-card">
-      <div className="image-carousel">
-        {displayImages.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Descripción de la imagen ${index + 1}`}
-            className={`product-image ${index === 0 ? "active" : ""}`}
-          />
-        ))}
-      </div>
-      <div className="product-description">
-        <h2>Nuestros Productos</h2>
-        <p>
-          Son elaborados con un proceso supervisado desde la preparación del
-          material, hasta los últimos acabados, contamos con la posibilidad de
-          ajustar tú producto acorde a las medidas solicitadas en tus proyectos.
-        </p>
-      </div>
-    </div>
-  );
+  const title = "Nuestros Productos";
+  const text = `Son elaborados con un proceso supervisado desde la preparación del
+    material, hasta los últimos acabados, contamos con la posibilidad de
+    ajustar tú producto acorde a las medidas solicitadas en tus proyectos.`;
+
+  return <SectionPH title={title} image={img} text={text} />;
 };
