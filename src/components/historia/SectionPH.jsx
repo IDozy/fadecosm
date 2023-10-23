965ea4a46ca1d44c96da7e5ae00b642046916892
Dozy <ahuatayc18_2@unc.edu.pe>
2023-10-23 15:18:17 -0500

    Producto-info , proyectos y fuentes

--- src/components/historia/SectionPH.jsx
+++ src/components/historia/SectionPH.jsx
@@ -2,18 +2,49 @@ import React from "react";
 import "./Historia.css";
 
 // Componente reutilizable para mostrar la historia o productos
-const SectionPH = ({ title, image, text }) => (
-  <div className="nuestra-historia-card">
-    <div className="nuestra-historia-image">
-      <img src={image} alt={`Imagen de ${title}`} />
-    </div>
-    <div className="nuestra-historia-text">
-      <div className="card-1" style={{ margin: "0 5px", paddingRight: "7%", height: "auto" }}>
-        <h2 className="text-title">{title}</h2>
-        <h4 className="text-info" style={{ marginBottom: ".5rem", paddingLeft: "1rem" }}>Fadedo SM</h4>
-        <p>{text}</p>
+const SectionPH = ({ title, image, text, clase }) => {
+  const shouldAddAosClass = clase;
+
+  return (
+    <div className="nuestra-historia-card">
+      <div
+        className={shouldAddAosClass ? "" : "nuestra-historia-image"}
+        data-aos={shouldAddAosClass ? "fade-right" : undefined}
+        data-aos-duration="3000"
+      >
+        {clase ? (
+          <div className="contenedor-imagen">
+            <div className="rotated-container">
+              <img src={image} alt={`Imagen de ${title}`} />
+            </div>
+          </div>
+        ) : (
+          <img src={image} alt={`Imagen de ${title}`} />
+        )}
+      </div>
+      <div className="nuestra-historia-text">
+        <div
+          className="card-1"
+          style={{ margin: "0 5px", paddingRight: "7%", height: "auto" }}
+        >
+          <h2 className="text-title">{title}</h2>
+          <h4
+            className="text-info"
+            style={{ marginBottom: ".5rem", paddingLeft: "1rem" }}
+          >
+            Fadedo SM
+          </h4>
+          <p>{text}</p>
+          {clase ? (
+            <div className="text-slide__buttons" style={{ marginTop: "2rem" }}>
+              <button className="button-slide">
+                <a href="/productos">Mas Productos</a>
+              </button>
+            </div>
+          ) : null}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 export default SectionPH;
--- src/components/productosCarrousel/NuestrosProductos.jsx
+++ src/components/productosCarrousel/NuestrosProductos.jsx
@@ -1,36 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import img from "../../images/Elaboracion/calidad1.jpg";
+import img1 from "../../images/Elaboracion/calidad2.jpg";
+import img2 from "../../images/Elaboracion/Elaboracion1.jpg";
+import img3 from "../../images/Elaboracion/Elaboracion2.jpg";
 
 import "../../components/historia/Historia.css";
-
-
+import SectionPH from "../historia/SectionPH";
 
 export const NuestrosProductos = () => {
-  const linkStyle = {
-    textDecoration: "none", 
-    color: "inherit", 
-  };
-  
+  const images = [img, img1, img2, img3];
+  const imageChangeInterval = 3000;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }, imageChangeInterval);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [images.length]);
+
+  const currentImage = images[currentIndex];
+
+  const title = "Nuestros Productos";
+  const text = `Son elaborados con un proceso supervisado desde la preparación del
+    material, hasta los últimos acabados, contamos con la posibilidad de
+    ajustar tú producto acorde a las medidas solicitadas en tus proyectos.`;
+
   return (
-    <div className="card-product-container">
-      <div className="card-product-content">
-        <div className="card-product-image">
-          <img src={img} alt="Empresa" />
-        </div>
-        <div className="card-product-text">
-          <h1>Nuestros Productos</h1>
-          <p>
-            Son elaborados con un proceso supervisado desde la preparación del
-            material, hasta los últimos acabados, contamos con la posibilidad de
-            ajustar tu producto acorde a las medidas solicitadas en tus proyectos.
-          </p>
-          <div className="card-product-button">
-          <button>
-              <a href="/productos" style={linkStyle}>Ver Productos</a>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+   
+    <SectionPH
+      title={title}
+      image={currentImage}
+      text={text}
+      clase={"imagen-prod"}
+    />
+
+  );
 };
