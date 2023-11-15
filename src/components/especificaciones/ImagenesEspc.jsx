1b0bdb379d277f4d489aa3c63f9fd7c933b6cbf6
gsoonc <109822183+gsoonc@users.noreply.github.com>
2023-11-14 23:28:46 -0500

    Se puede visualizar los titulos de la tabla

--- src/components/especificaciones/ImagenesEspc.jsx
+++ src/components/especificaciones/ImagenesEspc.jsx
@@ -1,25 +1,30 @@
 import React from "react";
+import TablaEspec from "./TablaEspec";
+
 
 const ImagenesEspc = ({ datosProductos }) => {
 
   if (!datosProductos) {
-    return <div>No hay datos disponibles</div>;
+    return <div>No hay datos de especificacionesdisponibles</div>;
   }
   
-
   return (
     <div>
-      {Object.keys(datosProductos).map((productoId) => {
-        const producto = datosProductos[productoId];
-        return (
-          <div key={productoId}>
-            <h3>{producto.nombre}</h3>
-            <img src={producto.imagen} alt={`Imagen de ${producto.nombre}`} />
-            <p>{producto.descripcion}</p>
-          </div>
-        );
-      })}
-    </div>
+    {Object.keys(datosProductos).map((productoId) => {
+      const producto = datosProductos[productoId];
+      console.log("Datos del producto:", producto);
+      return (
+        <div key={productoId}>
+          <h3>{producto.nombre}</h3>
+          <img src={producto.imagen} alt={`Imagen de ${producto.nombre}`} />
+          <p>{producto.descripcion}</p>
+
+          {/* Pasa las especificaciones al componente TablaEspec */}
+          <TablaEspec datosProductos={producto.especificaciones} />
+        </div>
+      );
+    })}
+  </div>
   );
 };
 
