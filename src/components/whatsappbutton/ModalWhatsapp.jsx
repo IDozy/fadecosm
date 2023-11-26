0556901946f3c28d8d6573b4c504b8ba0f14dbcc
Dozy <ahuatayc18_2@unc.edu.pe>
2023-11-25 20:15:59 -0500

    Modal responsive link

--- src/components/whatsappbutton/ModalWhatsapp.jsx
+++ src/components/whatsappbutton/ModalWhatsapp.jsx
@@ -1,26 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./modal.css";
 import imagen from "../../images/Svg/avatar-de-perfil.png";
 
-
 const ModalWhatsapp = ({ onClose }) => {
   const [message, setMessage] = useState("");
-    const [isAnimating, setIsAnimating] = useState(false);
-  
-    const handleClose = () => {
-      setIsAnimating(true);
-          setTimeout(() => {
-        onClose();
-      }, 1000);
-    };
-  
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [whatsappURL, setWhatsappURL] = useState("");
 
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth <= 768; // Puedes ajustar este valor según tus necesidades
+      const phoneNumber = "+51955445875"; // Reemplaza con el número de teléfono deseado
+      const encodedPhoneNumber = encodeURIComponent(phoneNumber);
+      const encodedMessageText = encodeURIComponent(message);
+
+      if (isMobile) {
+        setWhatsappURL(`https://wa.me/${encodedPhoneNumber}?text=${encodedMessageText}`);
+      } else {
+        setWhatsappURL(`https://web.whatsapp.com/send?phone=${encodedPhoneNumber}&text=${encodedMessageText}`);
+      }
+    };
+
+    handleResize(); // Llama a la función al montar el componente para establecer la URL inicial
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [message]);
+
+  const handleClose = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      onClose();
+    }, 1000);
+  };
 
   const handleSend = () => {
-    const phoneNumber = "+51955445875"; // Reemplaza con el número de teléfono deseado
-    const encodedPhoneNumber = encodeURIComponent(phoneNumber);
-    const encodedMessageText = encodeURIComponent(message);
-    const whatsappURL = `https://web.whatsapp.com/send?phone=${encodedPhoneNumber}&text=${encodedMessageText}`;
     window.open(whatsappURL, "_blank");
     onClose();
   };
@@ -54,6 +71,7 @@ const ModalWhatsapp = ({ onClose }) => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="10 -10 48 48"
           onClick={handleSend}
+          rel="noopener noreferrer"
         >
           <path
             d="M38.448 26.128 52.49 18.66a2.72 2.72 0 0 0 0-4.8l-27.736-14.756a2.719 2.719 0 0 0-1.826-.264l-6.712 1.38a2.719 2.719 0 0 0-1.7 4.192l6 8.824c.596.88 1.564 1.434 2.624 1.506l22.284 1.512-22.77 1.694a2.72 2.72 0 0 0-1.646.646l-6.15 9.074a2.719 2.719 0 0 0 1.742 4.196l6.144 1.17c.748.141 1.518.03 2.188-.166z"
