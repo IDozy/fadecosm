719795f96bd3d8a73b6c899daf49907086cdf420
Dozy <ahuatayc18_2@unc.edu.pe>
2023-11-28 18:35:29 -0500

    font about

--- src/components/infoAbout/InfoAbout.jsx
+++ src/components/infoAbout/InfoAbout.jsx
@@ -5,16 +5,16 @@ export const InfoAbout = () => {
   return (
     <div className="background-container">
       <div className="content">
+       
         <h1 className="content-h1">Empresa Cajamarquina</h1>
-        <p className="content-p">
-        Que busca generar un gran beneficio a nuestra provincia al producir y distribuir
-        variedad de productos de calidad, brindando el mejor servicio y asesoramiento
-        personalizado para todos tus proyectos.
-      </p>
+        <div className="about__content-p">
+          <p className="content-p">
+            Que busca generar un gran beneficio a nuestra provincia al producir
+            y distribuir variedad de productos de calidad, brindando el mejor
+            servicio y asesoramiento personalizado para todos tus proyectos.
+          </p>
+        </div>
       </div>
-     
-     
     </div>
   );
 };
-
