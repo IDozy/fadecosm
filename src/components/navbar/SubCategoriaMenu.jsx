76d76a48ce821f5dc76fd6a3a15cfe0c91b51b6c
gsoonc <109822183+gsoonc@users.noreply.github.com>
2023-12-02 15:44:58 -0500

    Funcion para que aparezca en el top de cada sub categoría seleccionada

--- src/components/navbar/SubCategoriaMenu.jsx
+++ src/components/navbar/SubCategoriaMenu.jsx
@@ -6,11 +6,19 @@ const formatSubcategoria = (subcategoria) => {
   return subcategoria.replace(/-/g, ' ');
 };
 const SubCategoriaMenu = ({ categoria, subcategorias }) => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Hace que el desplazamiento sea suave
+    });
+  };
+
+ 
   return (
     <ul className="subcategorias">
       {subcategorias.map((subcategoria) => (
         <li key={subcategoria}>
-          <Link className="tipo-size  "  to={`/productos/${encodeURIComponent(categoria)}/${encodeURIComponent(subcategoria)}`}>
+          <Link className="tipo-size" onClick={scrollToTop}  to={`/productos/${encodeURIComponent(categoria)}/${encodeURIComponent(subcategoria)}`}>
             {formatSubcategoria(subcategoria)}
             
           </Link>
